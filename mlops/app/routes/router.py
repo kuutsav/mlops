2a6e14365122ae0a545d640a7d5f509ad4dec98f
kuutsav <kumar.utsav@freshworks.com>
2021-10-11 13:28:38 +0530

    updated api tag

--- mlops/app/routes/router.py
+++ mlops/app/routes/router.py
@@ -5,4 +5,4 @@ from mlops.app.routes.endpoints import predict
 api_router = APIRouter()
 
 # predict endpoint
-api_router.include_router(predict.router, tags=["health_check"])
+api_router.include_router(predict.router, tags=["Predict"])
