90dd2db3e1269e9fa6aae8488f3fe6d2bbb21a75
utsav <krum.utsav@gmail.com>
2023-12-16 01:10:41 +0530

    costmetic edits

--- CONTRIBUTING.md
+++
@@ -1,7 +0,0 @@
-# Contribution guide
-
-- Create your branch from develop.
-- If you've added code that should be tested, add tests.
-- If you've changed APIs, update the documentation.
-- Ensure the test suite passes.
-- Make sure pre-commit passes before commiting.
--- mlops/utils/config.py
+++ mlops/utils/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parents[1]
 
-INPUT_DATASET_LOC = BASE_DIR.parent / "data_dev/Question_Classification_Dataset.csv"
+INPUT_DATASET_LOC = BASE_DIR.parent / "data/Question_Classification_Dataset.csv"
 TEST_SIZE = 0.2
 
 
@@ -12,9 +12,7 @@ def set_env_vars():
     with open(BASE_DIR / ".env", "r") as f:
         env_vars = f.readlines()
         env_vars = [
-            env_var.strip().split("=")
-            for env_var in env_vars
-            if not env_var.startswith("#") and env_var.strip()
+            env_var.strip().split("=") for env_var in env_vars if not env_var.startswith("#") and env_var.strip()
         ]
         for env_key, env_val in env_vars:
             os.environ[env_key] = env_val
