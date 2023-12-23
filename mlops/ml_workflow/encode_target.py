764d163c1f162c35550bd7b598e83defbf064bc4
Kumar Utsav <kumar.utsav@freshworks.com>
2023-12-24 00:34:47 +0530

    Update encode_target.py

--- mlops/ml_workflow/encode_target.py
+++ mlops/ml_workflow/encode_target.py
@@ -4,9 +4,7 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 
-def get_targetencoder_and_encoded_targets(
-    target: List[str],
-) -> Tuple[LabelEncoder, np.array]:
+def get_targetencoder_and_encoded_targets(target: List[str]) -> Tuple[LabelEncoder, np.array]:
     le = LabelEncoder()
     encoded_target = le.fit_transform(target)
     return le, encoded_target
